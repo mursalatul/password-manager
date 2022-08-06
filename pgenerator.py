04b9b075ae9be4fafab29588f9a8a4e16915be07
mursalatul <mursalatulislampallob@gmail.com>
2022-08-07 01:04:05 +0600

    dynamic password size & random number of elements
    Before this version we only have 6 digits password. but this time we can set the size of a password as we want. In the previous version we have 2 capital letters, 2 small letter, 2 numbers and 2 punch signs. But now which element will used how many times will be set randomly so that no one cant guess the password pattern.

--- pgenerator.py
+++ pgenerator.py
@@ -1,31 +1,42 @@
 import random
 
-def generator() -> str:
-	"""return 6 digit password
+# return random value in the range
+uppercaseLetter = lambda : chr(random.randint(ord('A'), ord('Z')))
+lowercaseletter = lambda : chr(random.randint(ord('a'), ord('z')))
+digit = lambda : str(random.randint(0, 9))
+punctuationSign = lambda : chr(random.randint(33, 47))
 
-	this function will return a 6 digit password where first
-	of the 2 letters are capital, second 2 letters are lower,
-	3rd 2 parts are digits and last 2 parts are punctuations
+def generator(passSize):
+	"""return "passSize" digit password
+
+	this function will return a "passSize" digit password
 
 	Args:
-		None
+		passSize (int) : password size
 	Return:
 		The str version of the password
 	Raises:
-		None
+		ValueError : if "passSize" is not a valid integer
 	"""
-	uppercaseLetter1 = chr(random.randint(ord('A'), ord('Z')))
-	uppercaseLetter2 = chr(random.randint(ord('A'), ord('Z')))
-	lowercaseletter1 = chr(random.randint(ord('a'), ord('z')))
-	lowercaseletter2 = chr(random.randint(ord('a'), ord('z')))
-	digit1 = chr(random.randint(ord('0'), ord('9')))
-	digit2 = chr(random.randint(ord('0'), ord('9')))
-	punctuationSign1 = chr(random.randint(33, 47))
-	punctuationSign2 = chr(random.randint(33, 47))
-	password = uppercaseLetter1 + uppercaseLetter2 + lowercaseletter1 + lowercaseletter2 + digit1 + digit2 + punctuationSign1 + punctuationSign2
-	password = list(password)
-	random.shuffle(password)
-	password = "".join(password)
-	return password
+	# list of possible password elements
+	passElements = ["uppercaseLetter", "lowercaseletter", "digit", "punctuationSign"]
+	password = []
+	for i in range(passSize):
+		# choicing a random element for the password and appending to password
+		element = random.choice(passElements)
+		match element:
+			case "uppercaseLetter":
+				password.append(uppercaseLetter())
+			case "lowercaseletter":
+				password.append(lowercaseletter())
+			case "digit":
+				password.append(digit())
+			case default:
+				password.append(punctuationSign())
+	random.shuffle(password) # shuffling the password
+	return "".join(password) # returning as a string
 
-print("password =", generator())
+p = int(input("How many password you want to generate: "))
+for _ in range(p):
+	size = int(input("Password size = "))
+	print("Pass :", generator(size))
