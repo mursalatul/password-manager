9b476523dbf8863f0579c9bc3e96ae90a174af5b
mursalatul <mursalatulislampallob@gmail.com>
2023-03-23 16:20:42 +0600

    added login section
    added login section to use features like save password. after login, no need to login again untill program is closed.

--- gui.py
+++ gui.py
@@ -5,6 +5,57 @@ import pgenerator
 import pstore
 
 # functions
+
+def logInAsAdmin():
+    """
+    Create a login pannel for log in as admin.
+    this section will work only when user open the app and want to use features
+    like save password for the first time. after log in one time it will not work again
+    untill the program is closed.
+    """
+    # functions of logInAsAdmin()
+    def checkUserPassOfAdmin():
+        f = open('admin.txt', 'r')
+        admin_username_from_txt = f.readline()[:-1] # removing last char cause it is \n at last of every line
+        admin_password_from_txt = f.readline()[:-1]
+        f.close()
+        if admin_username_from_txt == admin_username_entry.get() and admin_password_from_txt == admin_password_entry.get():
+            # seting asAdmin = True. from now no subwindow need login
+            global asAdmin
+            asAdmin = True
+            msgbox.showinfo("LOGIN", "Complete .")
+            login_wn.destroy()
+        else:
+            msgbox.showinfo("Error", "Wrong username or password")
+
+    # setup tkinter
+    login_wn = tk.Tk() # login_wn = login_window
+    login_wn.minsize(450, 450)
+    login_wn.maxsize(450, 450)
+    login_wn.configure(bg="#5DADE2")
+    login_wn.title("ADMIN Pannel")
+
+    # username
+    #         lable
+    admin_username_lable = tk.Label(login_wn, text="Username: ")
+    admin_username_lable.grid(row=0, column=0, padx=10, pady=10)
+    #         entry
+    admin_username_entry = tk.Entry(login_wn)
+    admin_username_entry.grid(row=0, column=1, padx=10, pady=10)
+
+    # password
+    #         lable
+    admin_password_lable = tk.Label(login_wn, text="Password: ")
+    admin_password_lable.grid(row=1, column=0, padx=10, pady=10)
+    #         entry
+    admin_password_entry = tk.Entry(login_wn)
+    admin_password_entry.grid(row=1, column=1, padx=10, pady=10)
+
+    # login button
+    tk.Button(login_wn, text="Login", command=checkUserPassOfAdmin).grid(row=3, columnspan=2, pady=10)
+    
+    login_wn.mainloop()
+
 def password_Insert():
     """
     clean password showing entry and display generated password
@@ -105,10 +156,16 @@ def savePassword():
 
 # save generated password
 def savepassword():
-    save_button.config(state=tk.DISABLED) # disabling the save_button so that no multiple save window open
-    savePassword()
+    if asAdmin:
+        save_button.config(state=tk.DISABLED) # disabling the save_button so that no multiple save window open
+        savePassword()
+    else:
+        logInAsAdmin()
 
 if __name__ == "__main__":
+    # admin
+    asAdmin = False # when user login asAdmin will be true
+
     # setup tkinter
     window = tk.Tk()
     # window.geometry("500X600")
